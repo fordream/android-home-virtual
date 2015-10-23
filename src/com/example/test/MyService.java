df1a803bc2274334036c901375bbd6e512495ad9
Vương Văn Trường <truongvv@atmarkcafe.org>
2015-10-23 14:54:21 +0700

    add

--- src/com/example/test/MyService.java
+++ src/com/example/test/MyService.java
@@ -1,25 +1,20 @@
 package com.example.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.vnp.androidvirtualkeypad.R;
 
-import android.app.Instrumentation;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.PixelFormat;
-import android.os.Handler;
 import android.os.IBinder;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.LinearLayout;
 
 public class MyService extends Service {
@@ -37,29 +32,29 @@ public class MyService extends Service {
 	}
 
 	private void create() {
+
+		OnClickListener clickListener = new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (v.getId() == R.id.btn_home) {
+
+				} else if (v.getId() == R.id.back) {
+					startBack();
+				} else if (v.getId() == R.id.home) {
+					startHomeLuncher();
+				}
+			}
+		};
 		WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
 
 		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 		LinearLayout layout = (LinearLayout) inflater.inflate(
 				R.layout.main_notif_mail, null);
 
-		layout.findViewById(R.id.back).setOnClickListener(
-				new OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						startBack();
-					}
-				});
-
-		layout.findViewById(R.id.home).setOnClickListener(
-				new OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						startHomeLuncher();
-					}
-				});
+		layout.findViewById(R.id.back).setOnClickListener(clickListener);
+		layout.findViewById(R.id.home).setOnClickListener(clickListener);
+		layout.findViewById(R.id.btn_home).setOnClickListener(clickListener);
 
 		WindowManager.LayoutParams params = new WindowManager.LayoutParams(
 				WindowManager.LayoutParams.WRAP_CONTENT,
@@ -67,6 +62,7 @@ public class MyService extends Service {
 				WindowManager.LayoutParams.TYPE_PHONE,
 				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
 				PixelFormat.TRANSLUCENT);
+
 		params.gravity = Gravity.TOP | Gravity.LEFT;
 		params.x = (windowManager.getDefaultDisplay().getWidth() - layout
 				.getWidth()) / 2;
@@ -76,15 +72,16 @@ public class MyService extends Service {
 		windowManager.addView(layout, params);
 	}
 
-	protected void startBack() {
-		new Handler().post(new Runnable() {
+	private void startBack() {
+		// ActivityManager am = (ActivityManager) this
+		// .getSystemService(ACTIVITY_SERVICE);
+		// List<ActivityManager.RunningTaskInfo> taskInfo =
+		// am.getRunningTasks(1);
+		// ComponentName componentInfo = taskInfo.get(0).topActivity;
+		//
+		// componentInfo.getPackageName();
 
-			@Override
-			public void run() {
-				Instrumentation m_Instrumentation = new Instrumentation();
-				m_Instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK);
-			}
-		});
+		
 
 	}
 
@@ -103,29 +100,30 @@ public class MyService extends Service {
 				.queryIntentActivities(new Intent(Intent.ACTION_MAIN)
 						.addCategory(Intent.CATEGORY_HOME),
 						PackageManager.MATCH_DEFAULT_ONLY)) {
-			if (resolveInfo.activityInfo.packageName.contains("launcher") || resolveInfo.activityInfo.packageName.contains("android")) {
+			if (resolveInfo.activityInfo.packageName.contains("launcher")
+					|| resolveInfo.activityInfo.packageName.contains("android")) {
 				launcher = resolveInfo.activityInfo.packageName;
 			}
 		}
 		return launcher;
 	}
 
-	public List<Intent> homes() {
-		ArrayList<Intent> intentList = new ArrayList<Intent>();
-		Intent intent = null;
-		final PackageManager packageManager = getPackageManager();
-		for (final ResolveInfo resolveInfo : packageManager
-				.queryIntentActivities(new Intent(Intent.ACTION_MAIN)
-						.addCategory(Intent.CATEGORY_HOME),
-						PackageManager.MATCH_DEFAULT_ONLY)) {
-			intent = packageManager
-					.getLaunchIntentForPackage(resolveInfo.activityInfo.packageName);
-			Log.e("TAGSSS", resolveInfo.activityInfo.packageName + " "
-					+ (intent == null));
-			intentList.add(intent);
-		}
-
-		return intentList;
-	}
+	// public List<Intent> homes() {
+	// ArrayList<Intent> intentList = new ArrayList<Intent>();
+	// Intent intent = null;
+	// final PackageManager packageManager = getPackageManager();
+	// for (final ResolveInfo resolveInfo : packageManager
+	// .queryIntentActivities(new Intent(Intent.ACTION_MAIN)
+	// .addCategory(Intent.CATEGORY_HOME),
+	// PackageManager.MATCH_DEFAULT_ONLY)) {
+	// intent = packageManager
+	// .getLaunchIntentForPackage(resolveInfo.activityInfo.packageName);
+	// Log.e("TAGSSS", resolveInfo.activityInfo.packageName + " "
+	// + (intent == null));
+	// intentList.add(intent);
+	// }
+	//
+	// return intentList;
+	// }
 
 }
